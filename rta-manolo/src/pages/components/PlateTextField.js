9bdb2af4a83d02950c1acce0ad3bba5412bb1398
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-05-25 16:16:57 +0800

    Updated Input fields in Seach Page

--- rta-manolo/src/pages/components/PlateTextField.js
+++ rta-manolo/src/pages/components/PlateTextField.js
@@ -5,7 +5,7 @@ function PlateTextField({value, onChange}) {
     return (
         <div>
             <div style={{position:"absolute", display:"flex"}}>
-                <Input  className='PlateNumber' style={{width:"30rem"}} value={value} onChange={onChange} />
+                <Input  className='PlateNumber' style={{width:"30rem", padding: 10, fontSize: 30}} value={value} onChange={onChange} />
                     {/* <div style={{fontSize:"3rem", marginTop: "-7rem"}}><h1>-</h1></div>
                 <TextField type='number' className='PlateNumber' style={{width:"13.5rem"}}></TextField>
                     <div style={{fontSize:"3rem", marginTop: "-7rem"}}><h1>-</h1></div>
